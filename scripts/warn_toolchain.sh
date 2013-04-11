5cfb1690e139b7bc7a7b5c07ead36e94c3c8ae7d
dlenz <dlenz>
2013-04-11 17:33:33 -0400

    Changeset: 12

--- scripts/warn_toolchain.sh
+++ scripts/warn_toolchain.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
+USERNAME=$(id -un)
+PRIMARYGROUP=$(id -gn)
+
 echo "Building buildroot will attempt to overwrite the Alix Toolchain in /opt."
 echo "This might fail unless you are logged in with an account in the same group."
+echo
+echo "You might try an 'sudo chown -R $USERNAME.$PRIMARYGROUP /opt/alix_toolchain' before continuing...."
+echo
 echo "Do you really want to try? Type YES."
 read answer
 if [ "$answer" = "YES" ]
